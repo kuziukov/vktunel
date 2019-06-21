c6ee1236e5e07d87c105ea2b1e8d1b9f3c4419e0
Dmitrii <dmitry.kuzyukov@arloid.com>
2019-06-21 15:27:28 +0300

    index and login page

--- src/web/routes.py
+++ src/web/routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from .views.index import index
+from .views.login import login
 
 web_bp = Blueprint('web', __name__, template_folder='./templates')
 
 web_bp.add_url_rule('/', 'index', index, methods=['GET'])
+web_bp.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])
