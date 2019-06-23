9fe81df3e238ed46247f10075d9b29ace0ff211b
Dmitrii Kuziukov <kuzukov.dima@gmail.com>
2019-06-23 12:53:53 +0300

    structure auth

--- src/web/routes.py
+++ src/web/routes.py
@@ -7,4 +7,4 @@ web_bp = Blueprint('web', __name__, template_folder='./templates')
 
 web_bp.add_url_rule('/', 'index', index, methods=['GET'])
 web_bp.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])
-web_bp.add_url_rule('/callback', 'callback', callback, methods=['GET', 'POST'])
+web_bp.add_url_rule('/callback', 'callback', callback, methods=['GET', 'POST']) 
