0078d8af9ce9014b276612821aa90e4276c96ed7
Dmitrii <dmitry.kuzyukov@arloid.com>
2019-06-24 18:19:39 +0300

    fixes routes

--- src/web/routes.py
+++ src/web/routes.py
@@ -10,3 +10,6 @@ web_bp.add_url_rule('/', 'index', index, methods=['GET'])
 web_bp.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])
 web_bp.add_url_rule('/callback', 'callback', callback, methods=['GET', 'POST'])
 web_bp.add_url_rule('/photos', 'photos', album_page, methods=['GET'])
+
+web_bp.add_url_rule('/community', 'community', None, methods=['GET'])
+web_bp.add_url_rule('/community/<community_id>/albums', 'albums', None, methods=['GET'])
