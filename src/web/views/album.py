1b4d2514dd9f00485ed57ca4148a0aaf1f64c59c
Dmitrii Kuziukov <kuzukov.dima@gmail.com>
2019-06-28 23:17:31 +0300

    fixes

--- src/web/views/album.py
+++ src/web/views/album.py
@@ -11,8 +11,12 @@ def album_page(community_id):
     api = API(g.user.access_token, v=5.95)
 
     community = api.groups.getById(group_id=community_id)[0]
-    response = api.photos.getAlbums(owner_id=f'-{community_id}', need_covers=1)
+    try:
+        response = api.photos.getAlbums(owner_id=f'-{community_id}', need_covers=1)
+        albums = response['items']
+        count = response['count']
+    except VkAPIError:
+        albums = []
+        count = 0
 
-    albums = response['items']
-    count = response['count']
     return render_template('album_page.html', albums=albums, count=count, datetime=datetime, community=community)
