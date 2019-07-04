42552c66ba227f295746eb7b93f7df0db3f3ea19
Dmitrii <dmitry.kuzyukov@arloid.com>
2019-07-04 15:10:17 +0300

    added size of file

--- src/utils/files.py
+++ src/utils/files.py
@@ -4,7 +4,7 @@ import math
 def convert_size(size_bytes) -> str:
     names_of_size = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
     if size_bytes == 0:
-       return "0B"
+        return "0B"
 
     log_of_bytes = int(math.log(size_bytes, 1000))
     number = round(size_bytes / math.pow(1000, log_of_bytes), 2)
