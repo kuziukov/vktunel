0ceb2cd97c5fa99102320a366538635c577369ba
Dmitrii <dmitry.kuzyukov@arloid.com>
2019-07-05 11:50:32 +0300

    changed

--- src/web/views/index.py
+++ src/web/views/index.py
@@ -18,26 +18,19 @@ def index():
         if form.validate_on_submit():
             data = form.search.data.split('/')
 
-            try:
+            if len(data) >= 4 and data[3]:
                 mask = data[3]
-            except Exception:
-                return render_template('main_page.html',
-                                       error_message='Простите, но ваша ссылка указана не верно, пожалуйста укажите ссылку правильно.')
 
-            api = API(access_token=g.user.access_token, v=5.100)
-            response = api.utils.resolveScreenName(screen_name=mask)
+                api = API(access_token=g.user.access_token, v=5.100)
+                response = api.utils.resolveScreenName(screen_name=mask)
 
-            try:
-                type = response['type']
-                object_id = response['object_id']
-            except Exception:
-                return render_template('main_page.html',
-                                       error_message='Простите, но ваша ссылка указана не верно, пожалуйста укажите ссылку правильно.')
+                if 'type' in response and 'object_id' in response:
+                    type = response['type']
+                    object_id = response['object_id']
 
-            if type == 'group':
-                return redirect(url_for('web.albums', community_id=object_id))
+                    if type == 'group':
+                        return redirect(url_for('web.albums', community_id=object_id))
 
-            return render_template('main_page.html',
+        return render_template('main_page.html',
                                    error_message='Простите, но ваша ссылка указана не верно, пожалуйста укажите ссылку правильно.')
-
     return page
