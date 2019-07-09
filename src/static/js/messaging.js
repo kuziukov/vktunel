32f300df8fc2518e61d4d0a88042421c1925761f
Dmitrii Kuziukov <kuzukov.dima@gmail.com>
2019-07-09 19:31:48 +0300

    -r

--- src/static/js/messaging.js
+++ src/static/js/messaging.js
@@ -1,5 +1,4 @@
-// Your web app's Firebase configuration
-  var firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyBlhrjca3zMlH1KPpSj2o3YbxZHj13ik7o",
     authDomain: "wlusmru.firebaseapp.com",
     databaseURL: "https://wlusmru.firebaseio.com",
@@ -7,67 +6,64 @@
     storageBucket: "",
     messagingSenderId: "900983135054",
     appId: "1:900983135054:web:7728e4e4579aa38b"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+firebase.initializeApp(firebaseConfig);
 
-  function sendSubscriptionToBackEnd(subscription) {
-      $.post(
-          "{{ url_for('api.fcmsubscriptionpost') }}",
-          JSON.stringify(subscription)
-      );
-    }
+function sendSubscriptionToBackEnd(subscription) {
+    $.post(
+        "{{ url_for('api.fcmsubscriptionpost') }}",
+        JSON.stringify(subscription)
+    );
+}
 
-  function urlBase64ToUint8Array(base64String) {
-      const padding = '='.repeat((4 - base64String.length % 4) % 4);
-      const base64 = (base64String + padding)
+function urlBase64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
         .replace(/\-/g, '+')
-        .replace(/_/g, '/')
-      ;
-      const rawData = window.atob(base64);
-      return Uint8Array.from([...rawData].map((char) => char.charCodeAt(0)));
-    }
+        .replace(/_/g, '/');
+    const rawData = window.atob(base64);
+    return Uint8Array.from([...rawData].map((char) => char.charCodeAt(0)));
+}
 
-    function subscribeUserToPush(key) {
-      return navigator.serviceWorker.register('static/js/sw.js')
-      .then(function(registration) {
-        const subscribeOptions = {
-          userVisibleOnly: true,
-          applicationServerKey: urlBase64ToUint8Array(
-            'BE_nppho7w0rkGST4UV1o_ISr4ljQtUYMbf9NkGB2cFcwudSarSskpH7-NEhSbUEvwj004n3IZi130Xfu3GXNzE'
-          )
-        };
+function subscribeUserToPush(key) {
+    return navigator.serviceWorker.register('static/js/sw.js')
+        .then(function(registration) {
+            const subscribeOptions = {
+                userVisibleOnly: true,
+                applicationServerKey: urlBase64ToUint8Array(
+                    'BE_nppho7w0rkGST4UV1o_ISr4ljQtUYMbf9NkGB2cFcwudSarSskpH7-NEhSbUEvwj004n3IZi130Xfu3GXNzE'
+                )
+            };
+            return registration.pushManager.subscribe(subscribeOptions);
+        })
+        .then(function(pushSubscription) {
+            console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
+            sendSubscriptionToBackEnd(pushSubscription);
+            return pushSubscription;
+        });
+}
 
-        return registration.pushManager.subscribe(subscribeOptions);
-      })
-      .then(function(pushSubscription) {
-        console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
-        sendSubscriptionToBackEnd(pushSubscription);
-        return pushSubscription;
-      });
-    }
-
-  function requestPermission() {
-      return new Promise(function(resolve, reject) {
+function requestPermission() {
+    return new Promise(function(resolve, reject) {
         const permissionResult = Notification.requestPermission(function(result) {
-          // Поддержка устаревшей версии с функцией обратного вызова.
-          resolve(result);
+            // Поддержка устаревшей версии с функцией обратного вызова.
+            resolve(result);
         });
         if (permissionResult) {
-          permissionResult.then(resolve, reject);
+            permissionResult.then(resolve, reject);
         }
-      })
-      .then(function(permissionResult) {
-        if (permissionResult !== 'granted') {
-          throw new Error('Permission not granted.');
-        }
-        subscribeUserToPush('key')
-      });
-      return true;
-    }
+    })
+        .then(function(permissionResult) {
+            if (permissionResult !== 'granted') {
+                throw new Error('Permission not granted.');
+            }
+            subscribeUserToPush('key')
+        });
+    return true;
+}
 
 
-  if ('serviceWorker' in navigator) {
+    if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('static/js/sw.js')
         .then(function() {
           console.log('SW registered');
