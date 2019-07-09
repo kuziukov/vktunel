58237dc0ac9400969a33f1b498d0dbbfe0c0a03b
Dmitrii Kuziukov <kuzukov.dima@gmail.com>
2019-07-09 19:22:05 +0300

    new feature in develop

--- src/static/js/sw.js
+++ src/static/js/sw.js
@@ -2,7 +2,8 @@ self.addEventListener('push', function(event) {
   var message = JSON.parse(event.data.text()); //
   event.waitUntil(
     self.registration.showNotification(message.title, {
-      body: message.body,
+        body: message.body,
+        icon: 'http://localhost:5000/static_folder/clouds.png'
     })
   );
 });
