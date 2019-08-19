3c9db1768482d3a03cb4586304e0ae880cc3b930
Dmitrii <dmitry.kuzyukov@arloid.com>
2019-08-19 11:55:38 +0300

    deleted

--- src/web/routes.py
+++ src/web/routes.py
@@ -3,15 +3,7 @@ from flask import (
     render_template
 )
 from .views import (
-    index,
-    login,
-    callback,
-    community_page,
-    album_page,
-    task_page,
-    task_post,
-    file_download,
-    support_page
+    file_download
 )
 from api.auth.before_request import before_request
 
@@ -19,18 +11,6 @@ from api.auth.before_request import before_request
 web_bp = Blueprint('web', __name__, template_folder='./templates')
 web_bp.before_app_request(before_request)
 
-
-web_bp.add_url_rule('/', 'index', index, methods=['GET', 'POST'])
-web_bp.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])
-web_bp.add_url_rule('/callback', 'callback', callback, methods=['GET', 'POST'])
-
-web_bp.add_url_rule('/tasks', 'tasks', task_page, methods=['GET'])
-
-web_bp.add_url_rule('/community', 'community', community_page, methods=['GET'])
-web_bp.add_url_rule('/community/<community_id>/albums', 'albums', album_page, methods=['GET'])
-web_bp.add_url_rule('/community/<community_id>/albums/<album_id>', 'task_post', task_post, methods=['GET'])
-
-web_bp.add_url_rule('/support', 'support', support_page, methods=['GET'])
 
 web_bp.add_url_rule('/files/<task_id>', 'file_download', file_download, methods=['GET'])
 
--- src/web/views/login.py
+++
@@ -1,8 +0,0 @@
-from flask import redirect
-from cores.objects import VKAuth
-
-
-def login():
-    auth = VKAuth('friends, photos, email, groups, offline')
-    return redirect(auth.url)
-
